270c8dd11ba95ac43d1332bce163c8370b346ffc
Tainara <tainararobertacarvalho@gmail.com>
2025-12-02 17:24:31 -0300

    feat: open_link basic

--- backend/tools/web_tools.py
+++ backend/tools/web_tools.py
@@ -1,24 +1,43 @@
 from agno.tools import tool
-import requests
 from bs4 import BeautifulSoup
-from agno.agent import Tool
+import requests
 
-
-BASE_URL = "https://www.ifsudestemg.edu.br/barbacena"
+BASE_URL = 'https://www.ifsudestemg.edu.br/barbacena'
 
 # Funções que o Agente usará. 
 
-""" 
+''' 
     Usa o decorator @Tool para transformar uma função Python comum em uma ferramenta 
     formal que o LLM consegue entender e usar.
-"""
+'''
 
-@tool(name="open_link", 
-      description="Abre um URL e retorna o texto principal da página. Útil para ler o conteúdo de uma notícia ou página específica.")
+@tool(name='open_link', 
+      description='Abre um URL e retorna o texto principal da página. Útil para ler o conteúdo de uma notícia ou página específica.')
 def open_link(url: str) -> str:
-    return ""
+    '''
 
-@tool(name="site_search", 
-      description="Realiza uma busca por um termo específico no site do Campus Barbacena. Retorna uma lista de títulos e URLs.")
+    Arg:
+        url (str): Link da página a ser aberta.
+    '''
+    try:
+        full_url = f"{BASE_URL}/{url.lstrip('/')}"
+        
+        response = requests.get(full_url, timeout=15)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Extrai somente o texto visual
+        text = soup.get_text(separator='\n', strip=True)
+        if not text:
+            return 'Erro ao acessar conteúdo da url {url}.'
+        
+        return text 
+    
+    except Exception as e: 
+        return f'Erro ao acessar o link {url}: {e}'
+
+@tool(name='site_search', 
+      description='Realiza uma busca por um termo específico no site do Campus Barbacena. Retorna uma lista de títulos e URLs.')
 def site_search(query: str) -> str:
-    return ""
+    return ''
