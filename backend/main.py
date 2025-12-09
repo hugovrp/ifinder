22a8537a85d47f369a77d6140bb727bd6035f97b
Vinícius Silva <code.vinni@gmail.com>
2025-12-08 21:54:41 -0300

    feat: API routes to generate IDs and get conversation data

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from agent_core import ChatAgent
+import uuid
 
 app = Flask(__name__, static_folder='../frontend', static_url_path='')
 CORS(app)
@@ -11,8 +12,45 @@ chat_agent = ChatAgent()
 def home():
     return send_file('../frontend/index.html')
 
+@app.route('/auth/generate', methods=['GET'])
+def generate_user_id():
+    """ Gera um UUID novo para um usuário.
+    """
+    user_id = str(uuid.uuid4())
+    return jsonify({ "user_id": user_id }), 201
+
+@app.route('/sessions/generate', methods=['POST'])
+def generate_session_id():
+    """ Gera um ID de sessão usando um novo UUID e o ID de um usuário.
+    """
+    data = request.get_json()
+    user_id = data.get("user_id")
+
+    if not user_id:
+        return jsonify({"error": "ID de usuário não fornecido."}), 400
+
+    session_id = str(uuid.uuid4())
+    return jsonify({ "session_id": f"{user_id}---{session_id}" }), 201
+
+@app.route('/sessions/get', methods=['POST'])
+def get_session_conversation():
+    """ Obtem a conversa entre o agente e o usuário em uma sessão específica
+    """
+    data = request.get_json()
+    session_id = data.get("session_id")
+
+    if not session_id:
+        return jsonify({"error": "ID de sessão não fornecido."}), 400
+
+    messages = chat_agent.agno_agent.get_chat_history(session_id=session_id)
+    messages_list = [ {"role": m.role, "content": m.content} for m in messages ]
+    
+    return jsonify({ "chat": messages_list }), 200
+    
+
 @app.route('/chat', methods=['POST'])
 def handle_chat():
+    
     data = request.get_json()
     user_prompt = data.get('prompt')
     session_id = data.get("session_id")
@@ -28,8 +66,9 @@ def handle_chat():
         return jsonify({
             "response": agent_response
         })
+    
     except Exception as e:
         return jsonify({"error": f"Erro interno do Agente: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5050)
